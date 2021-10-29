03a8e87f7943533418ce8b5af79075a198bd3bd2
mohamed ramadan <mr01028760097@gmail.com>
2021-10-29 17:54:51 +0200

    Add counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react'
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }handleCount
+  handleCountUp=()=>{
+    this.setState({count:this.state.count+1})
+  }
+  handleCountDown=()=>{
+    this.setState({count:this.state.count-1})
+  }
 
-function App() {
-  return (
-   <React.Fragment>
-     <h1 className="pt-5">App works</h1>
-     <div className="counting">here add state</div>
-   </React.Fragment>
-  );
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container text-center">
+          <button onClick={this.handleCountUp} className="my-4 btn btn-outline-success">Count Up</button>
+          <h1>{this.state.count}</h1>
+          <button onClick={this.handleCountDown} className="my-4 btn btn-outline-danger">Count Down</button>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
