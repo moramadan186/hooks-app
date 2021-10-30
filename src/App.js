a6d733c9d4728acf380e89d6ef1a32c39c3eed07
mohamed ramadan <mr01028760097@gmail.com>
2021-10-31 00:14:01 +0200

    add useState and reduce the code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import React, { Component } from 'react'
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0
-    }
-  } handleCount
-  handleCountUp = () => {
-    this.setState({ counter: this.state.counter + 1 })
-  }
-  handleCountDown = () => {
-    this.setState({ counter: this.state.counter - 1 })
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <div className="container text-center">
-          <button onClick={this.handleCountUp} className="my-4 btn btn-outline-success">Count Up</button>
-          <h1>{this.state.counter}</h1>
-          <button onClick={this.handleCountDown} className="my-4 btn btn-outline-danger">Count Down</button>
-        </div>
-      </React.Fragment>
-    );
-  }
+import React,{useState} from 'react'
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <React.Fragment>
+      <div className="container text-center">
+        <button onClick={() => setCounter(c => c + 1)} className="my-4 btn btn-outline-success">Count Up</button>
+        <h1>{counter}</h1>
+        <button onClick={() => setCounter(c => c - 1)} className="my-4 btn btn-outline-danger">Count Down</button>
+      </div>
+    </React.Fragment>
+  );
 }
-
-export default App;
+ 
